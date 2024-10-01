ffde930463a8db5a8bf9177b402d39fd96db3e70
Kobi-jan <jkobi002@gmail.com>
2024-10-01 20:21:52 +0300

    Done

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Setup the MySQL connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+// Test the database connection
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+    process.exit(1);
+  }
+  console.log('Connected to the MySQL database.');
+});
+
+app.get('/', (req, res) => {
+  res.send('Database connection is successful!');
+});
+
+
+// Question 1 goes here
+app.get('/patients', (req, res) => {
+  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients';
+  db.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(results);
+  });
+});
+
+// Question 2 goes here
+app.get('/providers', (req, res) => {
+  const query = 'SELECT first_name, last_name, provider_specialty FROM providers';
+  db.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(results);
+  });
+});
+
+// Question 3 goes here
+app.get('/patients/filter', (req, res) => {
+  const firstName = req.query.first_name;
+  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients WHERE first_name = ?';
+  db.query(query, [firstName], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(results);
+  });
+});
+
+// Question 4 goes here
+app.get('/providers/specialty', (req, res) => {
+  const specialty = req.query.specialty;
+  const query = 'SELECT first_name, last_name, provider_specialty FROM providers WHERE provider_specialty = ?';
+  db.query(query, [specialty], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(results);
+  });
+});
+
+// listen to the server
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`server is runnig on http://localhost:${PORT}`)
+})
